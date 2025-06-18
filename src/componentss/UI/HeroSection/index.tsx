2ad5d547aa68c716b83240e697441e1f9e7a9f26
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-06-18 16:40:47 +0000

    up

--- src/componentss/UI/HeroSection/index.tsx
+++ src/componentss/UI/HeroSection/index.tsx
@@ -20,7 +20,7 @@ const HeroSection = () => {
       <Inner>
         <Pill>
         <ChevronLeft className="opacity-80" />
-          <span className="bg-red-500">Stage-Link</span>
+          <span className="text-2xl">Stage-Link</span>
           <ChevronRight />
         </Pill>
 
