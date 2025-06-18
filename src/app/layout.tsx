0323f997b2b1f0157965f89a668237bc1dd256ad
Auguste-Houndjago <augustehoundjago@gmail.com>
2025-06-18 16:13:33 +0000

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Stage-Link',
-  description: 'Building the future of banking',
+  description: 'Stage Management Platform',
 };
 
 export default function RootLayout({
